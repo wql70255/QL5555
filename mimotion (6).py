5c45d27169b533719bad55e8a6b8803cb6223566
wql70255 <48751075+wql70255@users.noreply.github.com>
2021-08-18 20:46:30 +0800

    Update mimotion (6).py

--- mimotion (6).py
+++ mimotion (6).py
@@ -90,8 +90,8 @@ def main(_user, _passwd, _step):
         return "user and passwd not empty！"
 
     if _step == '':
-        print("已设置为随机步数（1000-10000）")
-        _step = str(random.randint(1000, 10000))
+        print("已设置为随机步数（30000-35000）")
+        _step = str(random.randint(30000, 35000))
     login_token, userid = login(_user, password)
     if login_token == 0:
         print("登陆失败！")
